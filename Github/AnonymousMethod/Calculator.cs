28cf7f1710ff825c95e71bbef2c892f5cae5798a
rspoojara <98145880+rspoojara@users.noreply.github.com>
2022-02-10 22:30:24 +0530

    AnnonymousMethod
    AnnonymousMethod 10/02/22

---
+++ Github/AnonymousMethod/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethod
+{
+    delegate int ComputeHandler(int a,int b);
+    class Calculator
+    {
+        public int Add(params int[] arr)
+        {
+            int result = 0;
+
+            foreach (int i in arr)
+            {
+                result += i;
+            }
+            return result;
+        }
+
+        public int Compute(int a,int b, ComputeHandler objD)
+        {
+            int result = -1;
+
+            result = objD(a,b);
+            return result;
+        }
+    }
+}
---
+++ Github/AnonymousMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnonymousMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calculator objCalc = new Calculator();
+
+            int result;
+
+            result = objCalc.Add();
+            result = objCalc.Add(10);
+            result = objCalc.Compute(100,200,Program.Multiply);
+
+        }
+
+        private static int Multiply(int a, int b)
+        {
+            return a * b;   
+        }
+    }
+}
